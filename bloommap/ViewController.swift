2c8f11cb9ae99c2842f252a2c61c30ca29528d1d
keisei1092 <keiseis1092@gmail.com>
2018-03-24 12:51:44 +0900

    Initial Commit

---
+++ bloommap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bloommap
+//
+//  Created by Keisei Saito on 2018/03/24.
+//  Copyright © 2018 Keisei Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
